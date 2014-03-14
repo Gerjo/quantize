03d911b7e71d27e6123324835e737697b405f81e
Gerjo <gerjoo@gmail.com>
2014-03-14 16:30:17 +0100

    More stub

--- Quantize/Tree.h
+++ Quantize/Tree.h
@@ -6,9 +6,29 @@
 //  Copyright (c) 2014 Quantize. All rights reserved.
 //
 
-#ifndef Quantize_Tree_h
-#define Quantize_Tree_h
+#pragma once
+
+#include "Math/Vector3.h"
+#include "Tribox.h"
+#include "Tools.h"
+#include "Model.h"
+
+using namespace Furiosity;
+
+class Tree {
+public:
+    Tree() {
+    
+    }
+    
+    void insert(VertexData v1, VertexData v2, VertexData v3) {
+    
+    }
+
+};
 
 
-
-#endif
+struct Box {
+    Vector3 center;
+    Vector3 hsize;
+};
