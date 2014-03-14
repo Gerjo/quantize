6b44f7d9098eb8a1ba1b2fd1a7ec9a9c7d4356f7
Gerjo <gerjoo@gmail.com>
2014-03-14 16:30:09 +0100

    stub for tree

---
+++ Quantize/Tree.h
@@ -0,0 +1,14 @@
+//
+//  Tree.h
+//  Quantize
+//
+//  Created by Gerard Meier on 14/03/14.
+//  Copyright (c) 2014 Quantize. All rights reserved.
+//
+
+#ifndef Quantize_Tree_h
+#define Quantize_Tree_h
+
+
+
+#endif
---
+++ Quantize/Tribox.h
@@ -0,0 +1,14 @@
+//
+//  Tribox.h
+//  Quantize
+//
+//  Created by Gerard Meier on 14/03/14.
+//  Copyright (c) 2014 Quantize. All rights reserved.
+//
+
+#ifndef Quantize_Tribox_h
+#define Quantize_Tribox_h
+
+
+
+#endif
