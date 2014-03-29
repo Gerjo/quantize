6e6f4dfa74d2fecd67c9f67b3af3a1c77a4b15d7
Gerjo <gerjoo@gmail.com>
2014-03-29 19:18:19 +0100

    The KdTree code. This GIT client isn't optimal.

--- Quantize/KdTree.cpp
+++ Quantize/KdTree.cpp
@@ -7,3 +7,44 @@
 //
 
 #include "KdTree.h"
+
+
+Node::Node(std::deque<Photon> array, int axis)
+    : axis(axis)
+    , alpha(new Node())
+    , beta(new Node())
+{
+    int axes = 3; // Number of vector components.
+    
+    // Sort on axis
+    std::sort(array.begin(), array.end(), [axis] (const Photon& u, const Photon& v) {
+        return u.position[axis] < v.position[axis];
+    });
+    
+    // Middle index
+    int middle = array.size() * 0.5;
+    
+    // Median photon
+    photon = array[middle];
+    
+    std::deque<Photon> a, b;
+    
+    // Split array into two segments
+    for(size_t i = 0; i < array.size(); ++i) {
+        if(i < middle) {
+            a.push_back(array[i]);
+        } else if(i > middle) {
+            b.push_back(array[i]);
+        }
+    }
+    
+    // Expand the left branch
+    if( ! a.empty()) {
+        alpha = new Node(a, (axis + 1) % axes);
+    }
+    
+    // Expand the right branch
+    if(! b.empty()) {
+        beta = new Node(b, (axis + 1) % axes);
+    }
+}
